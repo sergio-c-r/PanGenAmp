19fdff6b7454e35dd7e0069d21c34cf8edc2b8f2
sergio-c-r <160860322+sergio-c-r@users.noreply.github.com>
2025-04-02 18:25:40 +0200

    Add files via upload

--- A.py
+++ A.py
@@ -1,58 +1,23 @@
-import argparse
-import os
-import subprocess
-import shutil
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
+input_file_name = "core_genes"
+output_file_name = "list.txt"
 
-def run_blast(input_file, genus, specific_name):
-    output_file = os.path.join(os.path.dirname(input_file), 'blast_seq.txt')
-    blast_command = [
-        'blastn', '-query', input_file, '-db', 'nt', '-out', output_file,
-        '-outfmt', '6 sseqid sseq',
-        '-entrez_query', f'not {genus} {specific_name}[Organism]',
-        '-task', 'blastn', '-remote'
-    ]
-    subprocess.run(blast_command, check=True)
-    return output_file
+try:
+    with open(input_file_name, 'r') as input_file:
+        second_words = []
 
-def create_multifasta(blast_output):
-    fasta_output = os.path.splitext(blast_output)[0] + '.fasta'
-    with open(blast_output, 'r') as blast_file, open(fasta_output, 'w') as fasta_file:
-        for line in blast_file:
-            sseqid, sseq = line.strip().split('\t')
-            fasta_file.write(f'>{sseqid}\n{sseq}\n')
-    return fasta_output
+        for line in input_file:
+            words = line.split()
+            
+            if len(words) > 1:
+                # Append the second word to the list
+                second_words.append(words[1])
 
-def merge_fasta_files(input_dir, merged_fasta_filename):
-    fasta_files = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith('.fasta')]
-    with open(merged_fasta_filename, 'w') as merged_fasta_file:
-        for fasta_file in fasta_files:
-            with open(fasta_file, 'r') as infile:
-                shutil.copyfileobj(infile, merged_fasta_file)
+    with open(output_file_name, 'w') as output_file:
+        # Write the second words to the output file
+        output_file.write('\n'.join(second_words))
 
-def main():
-    parser = argparse.ArgumentParser(description="Run BLAST and create multifasta")
-    parser.add_argument('-i', '--input', required=True, help="Path to the input file")
-    parser.add_argument('-g', '--genus', required=True, help="Genus name to exclude")
-    parser.add_argument('-s', '--specific_name', required=True, help="Specific name to exclude")
-    
-    args = parser.parse_args()
-    
-    input_file = args.input
-    genus = args.genus
-    specific_name = args.specific_name
-    
-    blast_output = run_blast(input_file, genus, specific_name)
-    
-    multifasta_file = create_multifasta(blast_output)
-    
-    input_dir = os.path.dirname(input_file)
-    merged_fasta_filename = os.path.join(input_dir, 'merged_sequences.fasta')
-    merge_fasta_files(input_dir, merged_fasta_filename)
-
-if __name__ == "__main__":
-    main()
-
-<
+    print(f"Codes have been extracted to '{output_file_name}'.")
+except FileNotFoundError:
+    print(f"File '{input_file_name}' not found.")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
